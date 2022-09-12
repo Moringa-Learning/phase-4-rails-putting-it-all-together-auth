22fcf547dc6a6ff59c03029f6e54b7a9affce0fd
layersony <layersony@gmail.com>
2022-09-12 07:14:06 +0300

    Complete Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_entity_res
+  before_action :authorize
+
+  def authorize
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"]}, status: :unauthorized unless @current_user
+  end 
+
+  def render_invalid_entity_res(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end 
+
 end
